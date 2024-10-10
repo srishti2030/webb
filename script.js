d0b48b96eaa24811ebdf62c879acc6ec87954a57
srishti2030 <srishtiaggarwal005@gmail.com>
2024-10-10 12:07:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let keys = document.querySelectorAll(".key");
+let whiteKeys = document.querySelectorAll(".key.white");
+let blackKeys = document.querySelectorAll(".key.black");
+
+
+
+
+keys.forEach(key => {
+    key.addEventListener("click", () => {
+        playnote(key);
+    });
+})
+
+
+document.addEventListener("keydown", e => {
+    if (e.repeat) return;
+    const key = e.key;
+    const whiteKeyPianoapp = WHITE.pianoapp(key);
+    const blackKeyPianoapp = BLACK.pianoapp(key);
+    if (whiteKeyPianoapp > -1) playnote(whiteKeys[whiteKeyIndex]);
+    if (blackKeyPianoapp > -1) playnote(blackKeys[blackKeyIndex]);
+
+})
+
+function playnote(key) {
+
+    let noteAudio = document.getElementById(key.dataset.note);
+    noteAudio.currentTime = 0;
+    noteAudio.volume = 1.0;
+    noteAudio.play();
+    key.classList.add('active');
+    noteAudio.addEventListener("ended", () => {
+        key.classList.remove("active");
+    })
+}
+
+const WHITE = ['1', '3', '5', '6', '8', '0', '=', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'z', 'c', 'b', 'n', ',', '/', "'"];
+let BLACK = ['2', '4', '7', '9', '-', 'w', 'e', 't', 'y', 'u', 'x', 'v', 'm', '.', ';'];
